473b85918d9f3b1a78b4cbf19723e3b1b2c500d3
toghrulito <jabarov.t@gmail.com>
2023-09-20 16:17:57 -0400

    spring initial commit

--- Spring-03-Core-DI/src/main/java/org/app/App.java
+++ Spring-03-Core-DI/src/main/java/org/app/App.java
@@ -1,15 +1,14 @@
-package org.app.stereotype_annotation;
+package org.app;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
-
     public static void main(String[] args) {
 
-        ApplicationContext  container = new AnnotationConfigApplicationContext(ConfigCourse.class);
 
-        //container.getBean(Java.class).getTeachingHours();
+        ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class);
+        container.getBean(Java.class).getTeachingHours();
 
 
     }
---
+++ Spring-03-Core-DI/src/main/java/org/app/OfficeHours.java
@@ -0,0 +1,8 @@
+package org.app;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OfficeHours {
+    public int getHours(){return 5;};
+}
