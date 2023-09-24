34aa66fdeac9e6a3fd9dca9ebd4ad4a1fc618a38
jabar <jabarov.t@gmail.com>
2023-09-24 11:02:07 -0400

    Sprint core final

--- Spring-05-Core-BeanScope/src/main/java/org/app/App.java
+++ Spring-05-Core-BeanScope/src/main/java/org/app/App.java
@@ -1,7 +1,23 @@
 package org.app;
 
+import org.app.config.ConfigApp;
+import org.app.model.Comment;
+import org.app.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class App {
     public static void main(String[] args) {
+        Comment comment = new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring Framework");
 
+        ApplicationContext container = new AnnotationConfigApplicationContext(ConfigApp.class);
+
+        CommentService bean = container.getBean(CommentService.class);
+        CommentService bean2 = container.getBean(CommentService.class);
+
+
+        System.out.println(bean == bean2);
     }
 }
--- Spring-05-Core-BeanScope/src/main/java/org/app/service/CommentService.java
+++ Spring-05-Core-BeanScope/src/main/java/org/app/service/CommentService.java
@@ -4,9 +4,13 @@ import org.app.model.Comment;
 import org.app.proxy.CommentNotificationProxy;
 import org.app.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Scope("prototype")
+//@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class CommentService {
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
