34e7c2714584a313fd2fc5cbf90f49ad14dad56c
toghrulito <jabarov.t@gmail.com>
2024-02-10 22:22:26 +0000

    OnetoOne creating Employee and Department tables

---
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/entity/Department.java
@@ -0,0 +1,19 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "departments")
+@Data
+@NoArgsConstructor
+public class Department extends BaseEntity {
+    private String department;
+    private String division;
+
+    public Department(String department, String division) {
+        this.department = department;
+        this.division = division;
+    }
+}
