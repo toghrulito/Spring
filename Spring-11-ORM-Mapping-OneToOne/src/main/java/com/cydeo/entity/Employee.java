b9dff02c7dd577be694b7ac7c3c16dedee40e3aa
toghrulito <jabarov.t@gmail.com>
2024-02-11 14:38:49 +0000

    Mapping  and Cascading

--- Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/entity/Employee.java
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/entity/Employee.java
@@ -22,7 +22,15 @@ public class Employee extends BaseEntity {
     private Gender gender;
     private int salary;
 
-    public Employee(String firstName, String lastName, String email, LocalDate hireDate, Gender gender, int salary) {
+    @OneToOne(cascade = CascadeType.ALL)
+//    @OneToOne(cascade = {CascadeType.PERSIST,CascadeType.REMOVE})
+    @JoinColumn(name = "department_id")
+    private Department department;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+    public Employee(String firstName, String lastName, String email, LocalDate hireDate,  int salary,Gender gender) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
---
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/entity/Region.java
@@ -0,0 +1,26 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "regions")
+@Data
+@NoArgsConstructor
+public class Region extends BaseEntity{
+
+    private String region;
+    private String country;
+    @OneToOne(mappedBy = "region")
+    private Employee employee;
+
+
+    public Region( String region, String country) {
+        this.region = region;
+        this.country = country;
+    }
+}
+
